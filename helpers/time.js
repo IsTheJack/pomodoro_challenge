6f6d6cf517cd85863d7ec3fd7c3a0c709584b32e
Roberto <robertooliveiraestudo@gmail.com>
2016-05-31 18:03:49 -0300

    Add formater to time helper

--- helpers/time.js
+++ helpers/time.js
@@ -1,3 +1,15 @@
-export const minutesToSeconds = (minutes) => minutes * 60;
+export const minutesToSeconds = minutes => minutes * 60;
 
-export const secondsToMilliseconds = (seconds) => seconds * 1000;
+export const secondsToMilliseconds = seconds => seconds * 1000;
+
+// Seconds to format mm:ss
+export const formatToMinutesAndSeconds = seconds => {
+    const formatDigit = digit => digit < 10? ('0' + digit): digit
+
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    const formatedMinutes = formatDigit(min);
+    const formatedSeconds = formatDigit(sec);
+
+    return `${formatedMinutes}:${formatedSeconds}`;
+}
