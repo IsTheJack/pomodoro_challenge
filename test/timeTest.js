707a69d82749ef13f78472ae4ca5cb4d7ce8d996
Roberto <robertooliveiraestudo@gmail.com>
2016-06-01 08:38:34 -0300

    Add test suite

---
+++ test/timeTest.js
@@ -0,0 +1,14 @@
+import assert from 'assert';
+import {
+    minutesToSeconds,
+    secondsToMilliseconds,
+    formatToMinutesAndSeconds,
+} from '../helpers/time';
+
+describe('#Helper Test', function() {
+    describe('#minutesToSeconds', function() {
+        it('#Must to convert correctly', function() {
+            
+        });
+    });
+});
